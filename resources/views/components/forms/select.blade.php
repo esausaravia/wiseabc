a9f8b9d207781c5182275e260a3ebcb478a2a48e
Esau Saravia <esau.saravia.d@gmail.com>
2022-12-09 11:41:42 -0600

    componente forms.select con primera opción vacia

--- resources/views/components/forms/select.blade.php
+++ resources/views/components/forms/select.blade.php
@@ -23,6 +23,7 @@
   @endif
 
   <select name="{{$name}}" id="{{$id}}" class="block rounded-md shadow-sm h-12 xl:h-8 px-2 border border-black/10 bg-white text-gray-600" value="{{ old($name, $value) }}" >
+    <option value="">--</option>
     @foreach ( $options as $_key=>$_val)
       <option value="{{ $_key }}">{{ $_val }}</option>
     @endforeach
