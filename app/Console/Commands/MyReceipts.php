9e7986e1aba38ca141559e8d2981f8b0c25b0193
Esau Saravia <esau.saravia.d@gmail.com>
2023-02-13 19:32:43 -0600

    notas de ajustes/correcciones

--- app/Console/Commands/MyReceipts.php
+++ app/Console/Commands/MyReceipts.php
@@ -34,31 +34,52 @@ class MyReceipts extends Command
       $attendances = Attendance::whereDoesntHave('receipt')->get();
 
       foreach ($attendances as $attendance) {
-        if ($attendance->duracion >= 1800) {
-          $base = PaymentConcept::where('concept', 'base')->first()->amount;
-          $amount = $base;
-
-          if ($attendance->puntual) {
-            //se multiplica por 0.1 para convertir de minutos a horas
-            $amount += $base * 0.1;
-          }
-
-          $receipt = Receipt::create([
-            'attendance_id' => $attendance->id,
-            'status' => 'generated',
-            'amount' => $amount,
-          ]);
-
-          $receipt->conceptos()->attach(PaymentConcept::where('concept', 'Base')->first()->id, [
-            'amount' => $base,
-          ]);
-
-          if ($attendance->puntual) {
-            $receipt->conceptos()->attach(PaymentConcept::where('concept', 'Asistencia')->first()->id, [
-              'amount' => $base * 0.1,
-            ]);
-          }
+        if ($attendance->duracion < 1800) {
+          continue;
         }
-      }
+        $base = PaymentConcept::where('concept', 'LIKE', 'base')->first();
+        $asistencia = PaymentConcept::where('concept', 'LIKE', 'Asistencia')->first();
+        //FALTA Lealtad
+        //FALTA Grupal
+
+        $amount = $base->amount;
+
+        if ($attendance->puntual) {
+          //se multiplica por 0.1 para convertir de minutos a horas
+          $amount += $asistencia->amount;//?????????
+        }
+        /*
+        Falta obtener fecha de registro de profe y comparar contra fecha actual, si es mayor a 3 meses, se da el bono de lealtad
+        if ($profe->created_at < $hoy->menos('3 meses') ) {
+          $tiene_lealtad = true;
+        }
+        */
+
+        /* FALTA sumar cantidad grupal
+        if ( $clase->students->count() > 1 ) {
+          $tiene_grupal = true;
+        }
+        */
+
+        $receipt = Receipt::create([
+          'attendance_id' => $attendance->id,
+          'status' => 'generated',
+          'amount' => $amount,
+        ]);
+
+        $receipt->conceptos()->attach($base->id, ['amount' => $base->amount]);
+
+        //Asistencia = Puntualidad
+        if ($attendance->puntual) {
+          $receipt->conceptos()->attach($asistencia->id, ['amount' => $asistencia->amount]);
+        }
+        if ( $tiene_lealtad) {
+          //$receipt->conceptos()->attach($asistencia->id, ['amount' => $asistencia->amount]);
+        }
+        if ( $tiene_grupal ) {
+          //$receipt->conceptos()->attach($asistencia->id, ['amount' => $asistencia->amount]);
+        }
+
+      }//END foreach
     }
 }
