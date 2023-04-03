f9ecb63d22acf5096d3170401d8c3a9d1e1fb048
Esau Saravia <esau.saravia.d@gmail.com>
2023-04-03 12:55:25 -0600

    Ajustes comando MyReceipts

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -20,7 +20,7 @@ class Attendance extends Model
         return $this->belongsTo(Classroom::class);
     }
 
-    public function class(){
+    public function classroom(){
         return $this->belongsTo(Classroom::class);
     }
 
