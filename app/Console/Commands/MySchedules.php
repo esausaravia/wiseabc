a34f9c53dea59ca7025421c58b6146b02bf37fb2
Esau Saravia <esau.saravia.d@gmail.com>
2023-04-14 16:13:51 -0600

    MsApiController update MySchedules command update

--- app/Console/Commands/MySchedules.php
+++ app/Console/Commands/MySchedules.php
@@ -33,30 +33,64 @@ class MySchedules extends Command
     {
       $paraSemanas = 2; //cuantas semanas hacia adelante
       $enWeekdays = config('wiseabc.en_weekdays');//monday,tuesday,etc.
-      $hoy = now('-0600')->locale('es');
-      $hastaFecha = $hoy->copy()->addWeeks($paraSemanas);
+      $hoy = now('-0600');
+      $hoy_utc = $hoy->copy()->setTimezone('UTC');
+      $hastaFecha = $hoy_utc->copy()->addWeeks($paraSemanas)->addHour();
 
       $msApi = new MsApiController();
-      $token = $msApi->getAccessToken();
-      $Clases =  Classroom::where('status','active')
+      $Clases =  Classroom::with(['curso'])->where('status','active')
                     ->where('ends_at','>=', $hoy->format('Y-m-d') )->get();
 
       foreach($Clases as $clase) {
+        echo "Schedules for Classroom #{$clase->id}\n";
 
-        $schedules = Schedule::whereBetween('fechahora', [$hoy, $hastaFecha])->where('class_id', $clase->id)->orderBy('fechahora')->get();
+        /**
+         * Buscamos schedules sin registro en Microsoft Teams
+         */
+        $Schedules = Schedule::whereNull('teams_id')->where('class_id', $clase->id)->get();
+        foreach( $Schedules AS $schedule)
+        {
+          echo '  schedule->fechahora: '.$schedule->fechahora->format('Y-m-d H:i O').PHP_EOL;
+
+          $data = $msApi->createOnlineMeeting($clase->curso->name, $schedule->fechahora);
+
+          $teamsInfo = TeamsInfo::create([
+            'msid'=>$data['id'],
+            'link'=>$data['onlineMeeting']['joinUrl'],
+            'info'=>json_encode($data),
+            'report'=>""
+          ]);
+
+          $schedule->teams_id = $teamsInfo->id;
+          $schedule->save();
+          echo "  schedule->teams_id: {$schedule->teams_id}".PHP_EOL;
+        }
+
+        /**
+         * Buscamos schedules faltantes
+         */
+        $schedules = Schedule::whereBetween('fechahora', [$hoy_utc, $hastaFecha])->where('class_id', $clase->id)->orderBy('fechahora')->get();
+
         $schedules_count = $schedules->count();//cuantos hay registrados
+        echo "  schedules_count: {$schedules_count}\n";
+
         $schedules_need = (int)$clase->ritmo * $paraSemanas;//cuantos necesitamos
+        echo "  schedules_need: {$schedules_need}\n";
 
         $lastSchedule = $schedules->last();
         if ( !empty($lastSchedule) ) {
-          $lastSchedule = new Carbon($lastSchedule->fechahora, '-0600');
+          $lastSchedule = $lastSchedule->fechahora->copy();
         }
+
+        $lastSchedule->setTimezone('-0600');
+        echo '  lastSchedule: '.$lastSchedule->format('Y-m-d H:i O').PHP_EOL;
 
         while( $schedules_count < $schedules_need ) {
 
           $nextClass = $clase->sigFechaHora( $lastSchedule->addHour() );
+          echo '  nextClass: '.$nextClass->format('Y-m-d H:i O').PHP_EOL;
 
-          $data = $msApi->createOnlineMeeting($clase->curso->name, $nextClass, $token);
+          $data = $msApi->createOnlineMeeting($clase->curso->name, $nextClass);
 
           $teamsInfo = new TeamsInfo();
           $teamsInfo->msid = $data['id'];
@@ -68,13 +102,14 @@ class MySchedules extends Command
           $schedule = new Schedule();
           $schedule->class_id = $clase->id;
           $schedule->teams_id = $teamsInfo->id;
-          $schedule->fechahora = $nextClass;
+          $schedule->fechahora = $nextClass->copy()->setTimezone('UTC');
           $schedule->save();
 
           $lastSchedule = $nextClass;
           $schedules_count++;
         }
 
+        echo PHP_EOL;
       }//END foreach classes
       return "Cron job is working fine!";
 
