0723dafa5456cf7114b1cda9cccc2c1e28e82365
Esau Saravia <esau.saravia.d@gmail.com>
2023-05-18 15:08:48 -0600

    commands fix

--- app/Console/Commands/MySchedulesToAttendances.php
+++ app/Console/Commands/MySchedulesToAttendances.php
@@ -16,7 +16,7 @@ class MySchedulesToAttendances extends Command
    *
    * @var string
    */
-  protected $signature = 'my:atendance';
+  protected $signature = 'my:attendance';
 
   /**
    * The console command description.
@@ -32,33 +32,34 @@ class MySchedulesToAttendances extends Command
    */
   public function handle()
   {
-    $hoy = now('-0600');
     // Obtener horarios vencidos
-    $schedules = Schedule::with('classroom')->where('fechahora', '<', $hoy->copy()->subHour() )->get();
+    $schedules = Schedule::with(['classroom'])->where('fechahora', '<', now()->subHour() )->get();
 
     // Iniciar transacción
 
     foreach ($schedules as $schedule) {
       $classroom = $schedule->classroom;
+      $attendance = Attendance::create([
+        'user_id' => $classroom->teacher_id,
+        'class_id' => $classroom->id,
+        'teams_id' => null,
+        'fechahora' => $schedule->fechahora,
+        'duracion' => 2400,
+        'puntual' => true
+      ]);
       /*
-       * $msApi = new MsApiController();
-        $meeting = $msApi->getReport($classroom->teacher_id, $schedule['teams_id']);
+      $msApi = new MsApiController();
+      $meeting = $msApi->getReport($classroom->teacher_id, $schedule['teams_id']);
       //dd($meeting);
-        $total_attendance_in_seconds = $meeting['attendanceRecords'][0]['totalAttendanceInSeconds'];
-        $join_time = $meeting['attendanceRecords'][0]['attendanceIntervals'][0]['joinDateTime'];
-      */
-      $attendance = new Attendance;
-      $attendance->user_id = $classroom->teacher_id;
-      $attendance->class_id = $classroom->id;
-      $attendance->teams_id = null;
-      $attendance->fechahora = $schedule->fechahora;
-      /*
+      $total_attendance_in_seconds = $meeting['attendanceRecords'][0]['totalAttendanceInSeconds'];
+      $join_time = $meeting['attendanceRecords'][0]['attendanceIntervals'][0]['joinDateTime'];
+
       $attendance->duracion = $total_attendance_in_seconds;
       $attendance->puntual = ($join_time < Carbon::parse('11:05')) ? true : false;
-      */
-      $attendance->duracion = 2400;//40 mins
-      $attendance->puntual = true;
+
       $attendance->save();
+      */
+
 
       /*$team_info = TeamsInfo::where('id', $schedule['teams_id'])->first();
       $team_info->report = json_encode($meeting);
