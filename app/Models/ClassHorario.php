be89cfcfac56fedf98acd16ea267cb5b9e85486f
Esau Saravia <esau.saravia.d@gmail.com>
2023-11-16 13:42:36 -0600

    models relationships and methods return types

--- app/Models/ClassHorario.php
+++ app/Models/ClassHorario.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ClassHorario extends Model
 {
@@ -18,7 +19,7 @@ class ClassHorario extends Model
 
     protected $fillable = ['class_id', 'dia', 'hr'];
 
-    public function class()
+    public function class(): BelongsTo
     {
         return $this->belongsTo(Classroom::class);
     }
--- app/Models/billRegion.php
+++ app/Models/billRegion.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class billRegion extends Model
@@ -15,7 +16,7 @@ class billRegion extends Model
     /**
      * RelationShips
      */
-    public function billingPlans()
+    public function billingPlans(): HasMany
     {
         return $this->hasMany(BillingPlan::class);
     }
